c4b3fd4ace251b0878a3e790532630566896e3ee
JulianCambraia <juliancambraia@gmail.com>
2022-09-07 18:29:19 -0300

    log4j

--- src/main/java/br/com/juliancambraia/rabbitmq/rabbitmqexample/config/QueueConsummer.java
+++ src/main/java/br/com/juliancambraia/rabbitmq/rabbitmqexample/config/QueueConsummer.java
@@ -1,13 +1,15 @@
 package br.com.juliancambraia.rabbitmq.rabbitmqexample.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class QueueConsummer {
     @RabbitListener(queues = "${queue.name}")
     public void receive(@Payload String fileBody) {
-        System.out.printf("Message " + fileBody);
+        log.info("Message {} ", fileBody);
     }
 }
